8183b57707da79095b7f697b8a64b08b3e4b9537
unknown <ericandong6@gmail.com>
2023-07-10 15:41:37 +0200

    add recipe v1

---
+++ packages/frontend/src/components/SubmitButton.vue
@@ -0,0 +1,26 @@
+<template class="template">
+  <div>
+    <button v-on:click="warn('Le formulaire ne peut être soumis pour le moment.', $event)">
+        Soumettre le formulaire
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubmitButton",
+  methods: {
+  warn: function (message, event) {
+    if (event) {
+      event.preventDefault()
+    }
+    alert(message)
+  }
+},
+  setup() {
+    return {};
+  },
+};
+</script>
+<style>
+</style>
--- packages/frontend/src/pages/AddRecipePage.vue
+++ packages/frontend/src/pages/AddRecipePage.vue
@@ -1,26 +1,48 @@
 <template>
   <div>
-  
     <div class="text-h4">
     Ajouter une recette
     </div>
-    <p>
-    </p>
-   
+    <FormulateForm
+  v-model="formValues"
+>
+  <FormulateInput
+    name="name"
+    label="What is your name?"
+    validation="required"
+  />
+  <FormulateInput
+    name="age"
+    label="How old are you?"
+    validation="required|number|max:120"
+  />
+  <FormulateInput
+    type="submit"
+    label="Sign up"
+  />
+</FormulateForm>
+    <strong>Hello <em>{{ value }}</em>!</strong>
   </div>
+  <div><SubmitButton/></div>
 </template>
 
 <script>
+import { ref } from "vue";
+import SubmitButton from "../components/SubmitButton.vue";
 
 export default {
   name: "AddRecipePage",
- 
-  setup() {
-  
-    return {
-      
-    };
+  components: {
+    SubmitButton,
   },
+  setup() {
+    return {
+
 };
+},
+};
+
+
+
 </script>
 <style></style>
