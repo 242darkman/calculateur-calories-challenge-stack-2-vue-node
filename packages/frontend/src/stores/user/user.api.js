55c890d525e664e8d99b8240f130bb5e2ce8338f
242darkman <bvouvou@gmail.com>
2023-07-13 03:27:04 +0200

    feat: ajout d'une méthode permettant de récupérer un utilisateur spécifique

--- packages/frontend/src/stores/user/user.api.js
+++ packages/frontend/src/stores/user/user.api.js
@@ -9,3 +9,14 @@ export async function deleteUser({ id }) {
     console.error("Erreur lors de la suppression de l'utilisateur :", error);
   }
 }
+
+export async function getUser({ id }) {
+  const uri = `user/${id}`;
+  try {
+    const response = await api.get(uri);
+    const user = response.data;
+    return user;
+  } catch (error) {
+    console.error("Erreur lors de la suppression de l'utilisateur :", error);
+  }
+}
